38e473f1762d921e92dfa6680a8051834b9f671c
Muhammad Yafi <yafithekid212@gmail.com>
2016-01-01 16:10:41 +0700

    temp commit

--- src/main/java/com/yafithekid/instrumentation/example/Agent.java
+++ src/main/java/com/yafithekid/instrumentation/example/Agent.java
@@ -1,22 +1,10 @@
 package com.yafithekid.instrumentation.example;
 
-import java.io.IOException;
 import java.lang.instrument.Instrumentation;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Agent {
-
     public static void premain(String agentArgs, Instrumentation inst) {
-        try {
-            Runtime.getRuntime().exec("java -jar instrumentation-listener-1.0.jar");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         // registers the transformer
         inst.addTransformer(new SleepingClassFileTransformer());
     }
-
-
-
 }
--- src/main/java/com/yafithekid/instrumentation/example/Sleeping.java
+++ src/main/java/com/yafithekid/instrumentation/example/Sleeping.java
@@ -1,37 +1,41 @@
 package com.yafithekid.instrumentation.example;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Sleeping {
 
     public void randomSleep() throws InterruptedException {
-        // randomly sleeps between 500ms and 1200s
-        long randomSleepDuration = (long) (500 + Math.random() * 700);
+        //sleep for 100 ms
+        long randomSleepDuration = 100;
         System.out.printf("Sleeping for %d ms ..\n", randomSleepDuration);
         Thread.sleep(randomSleepDuration);
 
     }
 
-    public List<Integer> bigList(){
-        List<Integer> mboh = new ArrayList<>();
-
-        for(int i = 0; i < 1000000; i++){
-            mboh.add(i);
-        }
-        System.out.println("add list");
-        return mboh;
-    }
-
     public static void main(String[] args){
         Sleeping sleeping = new Sleeping();
         try {
+            long eltime = System.currentTimeMillis();
             sleeping.randomSleep();
-            sleeping.bigList();
+            System.out.println("total = "+(System.currentTimeMillis() - eltime));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
+
+//    public static void main(String[] args){
+//        int n = Integer.parseInt(args[0]);
+//        Sleeping sleeping = new Sleeping();
+//        try {
+//            long eltime = System.currentTimeMillis();
+//            for(int i = 0; i < n; i++){
+//                sleeping.randomSleep();
+//            }
+//            System.out.println("total = "+(System.currentTimeMillis() - eltime));
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
 }
+
