35f91fbce167699b15bbdcfc96691dd350b619a7
Muhammad Yafi <yafithekid212@gmail.com>
2016-01-21 10:06:03 +0700

    temp commit

---
+++ agent/src/main/java/com/yafithekid/instrumentation/agent/configs/Config.java
@@ -0,0 +1,45 @@
+package com.yafithekid.instrumentation.agent.configs;
+
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Config {
+    private List<MonitoredClass> classes;
+
+    public Config(){
+        this.classes = new ArrayList<>();
+    }
+    public void addMonitoredClass(MonitoredClass mc){
+        classes.add(mc);
+    }
+
+    public List<MonitoredClass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<MonitoredClass> classes) {
+        this.classes = classes;
+    }
+
+    public static void main(String []args) throws IOException {
+
+
+        ObjectMapper om = new ObjectMapper();
+        System.out.println(om.writeValueAsString(createDummy()));
+    }
+
+    public static Config createDummy(){
+        Config config = new Config();
+
+        MonitoredClass monitoredClass = new MonitoredClass("com.yafithekid.instrumentation.example.Sleeping");
+        MonitoredMethod method = new MonitoredMethod("randomSleep").memory().time();
+        monitoredClass.addMonitoredMethod(method);
+        monitoredClass.addMonitoredMethod(new MonitoredMethod("bigList"));
+        config.addMonitoredClass(monitoredClass);
+        return config;
+    }
+}
---
+++ agent/src/main/java/com/yafithekid/instrumentation/agent/configs/MonitoredMethod.java
@@ -0,0 +1,43 @@
+package com.yafithekid.instrumentation.agent.configs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MonitoredMethod {
+    public static final String MONITOR_TIME = "time";
+    public static final String MONITOR_MEMORY = "memory";
+
+    private String name;
+    private List<String> monitor;
+
+    public MonitoredMethod(String name){
+        this.name = name;
+        monitor = new ArrayList<>();
+    }
+
+    public MonitoredMethod time(){
+        monitor.add(MONITOR_TIME);
+        return this;
+    }
+
+    public MonitoredMethod memory(){
+        monitor.add(MONITOR_MEMORY);
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(List<String> monitor) {
+        this.monitor = monitor;
+    }
+}
