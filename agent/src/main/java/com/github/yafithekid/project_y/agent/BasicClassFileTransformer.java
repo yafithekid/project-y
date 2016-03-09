90152538b48dd47c80cecd4e674712443a883d70
Muhammad Yafi <yafithekid212@gmail.com>
2016-03-09 15:31:14 +0700

    cleanup transformer class

--- agent/src/main/java/com/github/yafithekid/project_y/agent/BasicClassFileTransformer.java
+++ agent/src/main/java/com/github/yafithekid/project_y/agent/BasicClassFileTransformer.java
@@ -31,42 +31,19 @@ public class BasicClassFileTransformer implements ClassFileTransformer {
      */
     public final int mCollectorPort;
 
-    private Config mConfig;
-
-    private MonitoredMethodSearchMap mMethodInvocationSearchMap;
     private MonitoredClassSearchMap mMonitoredClassSearchMap;
 
     public BasicClassFileTransformer(Config config){
-        mConfig = config;
         mCollectorHost = config.getCollector().getHost();
         mCollectorPort = config.getCollector().getPort();
-        mMethodInvocationSearchMap = new MonitoredMethodSearchMap(config.getClasses());
         mMonitoredClassSearchMap = new MonitoredClassSearchMap(config.getClasses());
     }
 
-//    public static final String COLLECTOR_CLIENT_CLASSNAME = "com.yafithekid.instrumentation.CollectorClient";
     @Override
     public byte[] transform(ClassLoader loader, String className, Class classBeingRedefined,
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         classfileBuffer = modifyByteCode(classfileBuffer,className);
         return classfileBuffer;
-    }
-
-    /**
-     * Insert bytecode for monitoring running time.
-     * @param cc class to be modified
-     * @param m the method to be altered
-     * @throws NotFoundException
-     * @throws CannotCompileException
-     */
-    private void insertRunningTime(CtClass cc,CtMethod m) throws NotFoundException, CannotCompileException{
-        //TODO overloaded method??
-        String fieldName = "elapsedTime_"+m.getName();
-        CtField ctField = new CtField(CtClass.longType,fieldName,cc);
-        cc.addField(ctField, CtField.Initializer.constant(5L));
-        m.insertBefore(fieldName+" = System.currentTimeMillis();");
-        m.insertAfter("{"+fieldName+" = System.currentTimeMillis() - "+fieldName+";"
-                + "System.out.println(\"Method Executed in ms: \" + "+fieldName+");}");
     }
 
     /**
@@ -87,21 +64,12 @@ public class BasicClassFileTransformer implements ClassFileTransformer {
             if (cc.isInterface()){
                 throw new IsInterfaceException(monitoredClass.getName());
             }
-//            System.out.println("modify class " + monitoredClass.getName());
             createDataCollectMethod(cc);
             for(MonitoredMethod method:monitoredClass.getMethods()) {
                 try {
                     CtMethod ctMethod = cc.getDeclaredMethod(method.getName());
-//                    System.out.println("trying to modify: "+monitoredClass.getName()+"#"+method.getName());
                     if (!isAbstract(ctMethod)){
-//                        System.out.println("modifying "+monitoredClass.getName()+"#"+method.getName());
-//                        createOverloadMethod(cc, ctMethod);
                         insertLocalVariables(cc,ctMethod);
-//                        if (method.isTrace()){
-//                            insertExpressionEditor(cc,ctMethod);
-//                        }
-//                    insertRunningTime(cc,ctMethod);
-//                    insertMemoryUsage(cc,ctMethod);
                         insertDataCollect(cc,ctMethod);
                     } else {
                         throw new IsAbstractMethodException(monitoredClass.getName(),method.getName());
@@ -114,7 +82,6 @@ public class BasicClassFileTransformer implements ClassFileTransformer {
             }
             ret = cc.toBytecode();
             cc.detach();
-//            System.out.println("modify class completed");
             return ret;
         } catch (NotFoundException e) {
             throw new ClassNotExistException(monitoredClass.getName());
@@ -125,24 +92,6 @@ public class BasicClassFileTransformer implements ClassFileTransformer {
             e.printStackTrace();
             return null;
         }
-    }
-
-
-    /**
-     * Insert bytecode for monitoring memory usage.
-     * @param cc class to be modified
-     * @param m the name of method to be monitored
-     * @throws NotFoundException
-     * @throws CannotCompileException
-     */
-    private void insertMemoryUsage(CtClass cc,CtMethod m) throws NotFoundException, CannotCompileException {
-        //TODO overloaded method??
-        String fieldName = "memoryUsage_"+m.getName();
-        CtField ctField = new CtField(CtClass.longType,fieldName,cc);
-        cc.addField(ctField, CtField.Initializer.constant(5L));
-        m.insertBefore(fieldName+" = Runtime.getRuntime().freeMemory();");
-        m.insertAfter("{"+fieldName+" = "+fieldName+" - Runtime.getRuntime().freeMemory();"
-                + "System.out.println(\"Memory usage is: \" + "+fieldName+");}");
     }
 
 
@@ -158,7 +107,6 @@ public class BasicClassFileTransformer implements ClassFileTransformer {
         String replacedLoadadClassName = loadedClassName.replace("/",".");
         if (mMonitoredClassSearchMap.exist(replacedLoadadClassName)){
             MonitoredClass mc = mMonitoredClassSearchMap.get(replacedLoadadClassName);
-//            System.out.println(replacedLoadadClassName+" found");
             try {
                 return modifyClass(mc);
             } catch (IsInterfaceException e) {
@@ -170,49 +118,6 @@ public class BasicClassFileTransformer implements ClassFileTransformer {
             }
         } else {
             return byteCode;
-        }
-    }
-
-    /**
-     * Add method for sending data via socket
-     * the first parameter of method is a string that will be given to socket
-     * to access the parameter, use "$1"
-     */
-    void createOverloadMethod(CtClass ctClass,CtMethod ctMethod){
-        try {
-            //add string as first parameter
-            CtClass[] parameterTypesArray = ctMethod.getParameterTypes();
-            List<CtClass> parameterTypes = new ArrayList<CtClass>();
-            parameterTypes.add(getClassString());
-            for(int i = 0; i < parameterTypesArray.length; i++){
-                parameterTypes.add(parameterTypesArray[i]);
-            }
-            int nParams = parameterTypes.size();
-            //construct $2...nParams
-            StringBuilder sb = new StringBuilder();
-            for(int i = 2; i <= nParams; i++){
-                if (i > 2) sb.append(",");
-                sb.append("$").append(i);
-            }
-            String methodBody;
-            if (ctMethod.getReturnType().equals(CtClass.voidType)){
-                methodBody = String.format("{%s(%s); System.out.println(\"dummy\");}",ctMethod.getName(),sb.toString());
-            } else {
-                methodBody = String.format("{ %s __ret = this.%s(%s); System.out.println(\"dummy\"); return __ret; }",ctMethod.getReturnType().getName(),
-                        ctMethod.getName(),sb.toString());
-            }
-            CtMethod dataCollectMethod;
-            CtClass[] params = new CtClass[parameterTypes.size()];
-            for(int i = 0; i < params.length; i++){
-                params[i] = parameterTypes.get(i);
-            }
-            dataCollectMethod = CtNewMethod.make(ctMethod.getReturnType(),ctMethod.getName(),
-                    params,ctMethod.getExceptionTypes(),methodBody,ctClass);
-            ctClass.addMethod(dataCollectMethod);
-        } catch (CannotCompileException e){
-            e.printStackTrace();
-        } catch (NotFoundException e) {
-            e.printStackTrace();
         }
     }
 
@@ -237,24 +142,6 @@ public class BasicClassFileTransformer implements ClassFileTransformer {
                 DATA_COLLECT_METHOD+"("+data+");" +
 //                "com.github.yafithekid.project_y.agent.Sender.get().send("+data+");" +
                 "}");
-    }
-
-    void insertExpressionEditor(CtClass cc,CtMethod m) throws NotFoundException,CannotCompileException {
-        m.instrument(new ExprEditor(){
-            @Override
-            public void edit(MethodCall methodCall) throws CannotCompileException {
-                if (mMethodInvocationSearchMap.exist(methodCall.getClassName(),methodCall.getMethodName())){
-                    try {
-                        methodCall.getMethod();
-                    } catch (NotFoundException e) {
-                        e.printStackTrace();
-                    }
-//                    methodCall.replace("{ $_ = $proceed($$); System.out.println(\"hore\"); }");
-                    methodCall.replace("{ $_ =  $proceed($$,__invocationId);}");
-                }
-
-            }
-        });
     }
 
     CtClass getClassString(){
