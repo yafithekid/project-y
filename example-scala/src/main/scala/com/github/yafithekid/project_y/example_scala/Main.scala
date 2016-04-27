c6c9b4532aef5c90b71f5db8b615b0615fde06ff
Muhammad Yafi <yafithekid212@gmail.com>
2016-04-27 16:55:07 +0700

    Add example scala project; remove gradle idea plugin

---
+++ example-scala/src/main/scala/com/github/yafithekid/project_y/example_scala/Main.scala
@@ -0,0 +1,10 @@
+package com.github.yafithekid.project_y.example_scala
+
+/**
+  * Created by yafi on 27-Apr-16.
+  */
+object Main {
+  def main(args: Array[String]): Unit ={
+    println("Hello world")
+  }
+}
